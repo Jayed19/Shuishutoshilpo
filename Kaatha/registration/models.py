799c79ef4c5c4dc4ba51618f8d2b6ce59e6df35c
jayed <jayed19@gmail.com>
2021-07-07 23:55:04 +0600

    Model and Image Upload by Admin Panael

--- Kaatha/registration/models.py
+++ Kaatha/registration/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class registrationpage(models.Model):
+    name=models.CharField(max_length=50,blank=False, null=False)
+    pic=models.ImageField(upload_to='registrationPic/',blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+    
