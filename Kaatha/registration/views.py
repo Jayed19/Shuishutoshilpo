190103b3bdc875d2a485f6e06a3fdc0de77c339f
jayed <jayed19@gmail.com>
2021-07-07 12:19:11 +0600

    List Sent from registration.view to template.registration html page

--- Kaatha/registration/views.py
+++ Kaatha/registration/views.py
@@ -6,6 +6,7 @@ def home(request):
     user={
         "name": "Jayed Ibrahim",
         "mobile":"01917858019",
-        "email":"jayed19@gmail.com"
+        "email":"jayed19@gmail.com",
+        "orderList":["RK001","GJ005","BD001"]
     }
     return render(request,'registration/registration.html',user)
