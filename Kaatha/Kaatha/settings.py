a4cb802c9fe483e321404b4f7e396bcb91111300
jayed <jayed19@gmail.com>
2021-07-07 16:34:01 +0600

    static folder create and css file added

--- Kaatha/Kaatha/settings.py
+++ Kaatha/Kaatha/settings.py
@@ -122,7 +122,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+STATIC_ROOT =  BASE_DIR / "static/assets/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
